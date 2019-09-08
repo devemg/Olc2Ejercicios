9de8ae9c369099239796d30c6d44ac9eb0298b68
EmelyGarcia <garciam.emm@gmail.com>
2019-09-08 16:19:23 -0600

    Ejercicio 18

---
+++ src/Analizador/E18/Atributo.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Analizador.E18;
+
+/**
+ *
+ * @author Emely
+ */
+public class Atributo {
+    private String valor;
+    private String op;
+
+    public Atributo(String valor, String op) {
+        this.valor = valor;
+        this.op = op;
+    }
+
+    
+    /**
+     * @return the valor
+     */
+    public String getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the op
+     */
+    public String getOp() {
+        return op;
+    }
+
+    /**
+     * @param op the op to set
+     */
+    public void setOp(String op) {
+        this.op = op;
+    }
+}
